1d058fc845151f362e64e73338c38ad02a9e5efc
Mikkel Nygaard <mnygaa15@student.aau.dk>
2015-11-23 12:22:07 +0100

    Ting og sager

--- code/p1-program.c
+++ code/p1-program.c
@@ -2,35 +2,47 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MAX_LECTURES 80
+
+
+/*typedef struct lecture {
+  char *room;
+} lecture; */
+
 typedef struct course {
   char *course;
   int lectures;
 } course;
 
 void initialPopulation(int inititalPopulationAmount, course *courses,
-                       course *population[inititalPopulationAmount][totalLectures], int arrayLength, int totalLectures);
+                       char* population[10][MAX_LECTURES], int arrayLength, int totalLectures);
 int getTotalLectures(course *courses, int arrayLength);
+void generateSchedule( course *courses, char* population[10][MAX_LECTURES], int arrayLength, int totalLectures, int populationCount);
 int getRandomCourse(course *courses, int arrayLength);
+void outputSchedule(char* population[10][MAX_LECTURES]);
+
 
 int main(void) {
   srand(time(NULL));
 
-  course courses[3];
+  int i;
 
-  course temp1 = {"LIAL", 25};
+  course courses[4];
+
+  course temp1 = {"LIAL", 20};
   courses[0] = temp1;
   course temp2 = {"IMP", 20};
   courses[1] = temp2;
-  course temp3 = {"PV", 12};
+  course temp3 = {"PV", 20};
   courses[2] = temp3;
-  course temp4 = {"PRJ", 35};
+  course temp4 = {"PRJ", 20};
   courses[3] = temp4;
 
-  int inititalPopulationAmount = 10, totalLectures, arrayLength = sizeof(courses);
+  int inititalPopulationAmount = 10, totalLectures, arrayLength = sizeof(courses) / sizeof(courses[0]);
 
   totalLectures = getTotalLectures(courses, arrayLength);
 
-  course population[inititalPopulationAmount][totalLectures];
+  char* population[inititalPopulationAmount][totalLectures];
 
   initialPopulation(inititalPopulationAmount, courses, population, arrayLength, totalLectures);
 
@@ -40,21 +52,20 @@ int main(void) {
     mutation();
     newPopulation();
   }
-
-  outputSchedule();
 */
+
+  outputSchedule(population);
+
   return 0;
 }
 
 void initialPopulation(int inititalPopulationAmount, course *courses,
-                       course *population[inititalPopulationAmount][totalLectures], int arrayLength, int totalLectures) {
-  int random, populationCount, lectureCount;
+                       char* population[10][MAX_LECTURES], int arrayLength, int totalLectures) {
+  int populationCount;
 
   for (populationCount = 0; populationCount < inititalPopulationAmount; populationCount++) {
-    for (lectureCount = 0; lectureCount < totalLectures; lectureCount++) {
-      random = getRandomCourse(courses, arrayLength);
-      population[populationCount][lectureCount] = courses[random].course;
-    }
+    printf("\n\n");
+    generateSchedule(courses, population, arrayLength, totalLectures, populationCount);
   }
 }
 
@@ -64,24 +75,54 @@ int getTotalLectures(course *courses, int arrayLength) {
   for (i = 0; i < arrayLength; i++) {
     totalLectures += courses[i].lectures;
   }
-
   return totalLectures;
+}
+
+void generateSchedule(course *courses, char* population[10][MAX_LECTURES], int arrayLength, int totalLectures, int populationCount) {
+  course tempCourses[arrayLength];
+  int i, lectureCount, random;
+
+  for (i = 0; i < arrayLength; i++) {
+    tempCourses[i] = courses[i];
+  }
+
+  for (lectureCount = 0; lectureCount < totalLectures; lectureCount++) {
+    random = getRandomCourse(tempCourses, arrayLength);
+    population[populationCount][lectureCount] = courses[random].course;
+  }
 }
 
 int getRandomCourse(course *courses, int arrayLength) {
   int random = (rand() % arrayLength);
-  int randomTemp = random;
+
+  while (1) {
+    if (courses[random].lectures > 0) {
+      courses[random].lectures--;
+      printf("SE: %d ", random);
+      return random;
+    } else {
+      random = (rand() % arrayLength);
+    }
+  }
+
+
+  /*int randomTemp = random;
 
   do {
-    if (courses[randomTemp].lectures > 0) {
-      courses[randomTemp].lectures--;
-      return random;
-    } else if (randomTemp == arrayLength) {
+    if (randomTemp == arrayLength) {
       randomTemp -= arrayLength;
-    } else
+    } else if (courses[randomTemp].lectures > 0) {
+      courses[randomTemp].lectures--;
+      printf("SE: %d ", randomTemp);
+      return randomTemp;
+    } else {
+      printf("OP: %d ", randomTemp);
       randomTemp++;
-  } while(randomTemp != random);
+    }
+  } while(randomTemp != 5);
+  */
 }
+
 
 /*void calculateFitness() {
 ​
@@ -99,8 +140,14 @@ void newPopulation() {
 
 
 }
-​
-void outputSchedule() {
-​
+​*/
+void outputSchedule(char* population[10][MAX_LECTURES]) {
+  int i, j;
+
+  for (i = 0; i < 10; i++) {
+    printf("\n\n\n");
+    for (j = 0; j < MAX_LECTURES; j++) {
+      printf("%s ", population[i][j]);
+    }
+  }
 }
-*/
