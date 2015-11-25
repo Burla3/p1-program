39b52372d58489611076c481133aba0af8949711
Jalf <benjaminboendergaard@hotmail.com>
2015-11-25 15:48:32 +0100

    Added example of the jsonParser

--- code/p1-program.c
+++ code/p1-program.c
@@ -1,9 +1,29 @@
-#include "hellofunc.h"
+#include <stdio.h>
+#include "jsonParser.h"
+
+void jsonExample();
 
 int main()
 {
-  // call a function in another file
-  myPrintHelloMake();
+  jsonExample();
 
   return 0;
+}
+
+void jsonExample() {
+  json_t *root, *value;
+
+  root = loadJSON("json/config.json");
+
+  if (root == NULL) {
+    perror("Error loading JSON ");
+  }
+
+  value = json_object_get(root, "check");
+
+  json_decref(root);
+
+  printf("%s\n", json_string_value(value));
+
+  json_decref(value);
 }
