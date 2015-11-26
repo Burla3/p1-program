2f6a263fdeebdcd51cdcaacb732fc910884a6341
Burla3 <jens@burla.dk>
2015-11-26 15:19:20 +0100

    Added selection function and sortPopulation function for qsort

--- code/p1-program.c
+++ code/p1-program.c
@@ -254,6 +254,30 @@ void outputSchedule(char ***population, int totalLectures) {
   printf("\n");
 }
 
+int sortPopulation(const void *ep1, const void *ep2) {
+  int result;
+  timetable *timetable1 = (timetable *)ep1,
+            *timetable2 = (timetable *)ep2;
+
+  if (timetable1->fitnessScore < timetable2->fitnessScore) {
+    result = 1;
+  } else if (timetable1->fitnessScore > timetable2->fitnessScore) {
+    result = -1;
+  } else {
+    result = 0;
+  }
+
+  return result;
+}
+
+void selection(const timetable population[], timetable newPopulation[]) {
+  int i;
+
+  for (i = 0; i < POPULATION_SIZE / 2; i++) {
+    newPopulation[i] = population[i];
+  }
+}
+
 /**
  * Breeds one offspring by randomly picking lectures from the first or second parent and putting them into the
  * offspring.
