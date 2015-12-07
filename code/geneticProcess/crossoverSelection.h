213ccfdd24b2c9f472c1502f5fe3fa1081bd3d14
Burla3 <jens@burla.dk>
2015-12-07 15:53:00 +0100

    Added selection function for crossover
    Added selection function for crossover

---
+++ code/geneticProcess/crossoverSelection.h
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "../structs.h"
+
+/**
+ * Selects two random population members for crossover.
+ *
+ * @param[in] population[] the population that two parents will be selected from randomly
+ * @param[in] currentPopulationSize the current size of the population which is used as a limit for selecting randomly
+ * @param[out] parent1 the first parent that is selected from the population
+ * @param[out] parent2 the second parent that is selected from the population
+ */
+void crossoverSelectionRandom(PopMember population[], int currentPopulationSize, PopMember parent1, PopMember parent2);
