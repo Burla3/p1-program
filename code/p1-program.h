67b0264f2320455edab95262575f003a338de2e3
Jalf <benjaminboendergaard@hotmail.com>
2015-12-07 08:43:35 +0100

    Cleaned up in main file

--- code/p1-program.h
+++ code/p1-program.h
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include "jsonParser.h"
 #include "jsonReadToStructs.h"
 #include "geneticProcess/geneticProcess.h"
+#include "structs.h"
 
 /**
  * Function used to load the initial configurations used in the rest of the program.
  */
-void initialConfiguration();
+void initialConfiguration(json_t *rootConfig);
+
+int getNumberOfStudies(json_t *rootConfig);
