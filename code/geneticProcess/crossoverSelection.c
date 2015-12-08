b6f34b6c320e8f4c37116ee987379359f48c5f29
Burla3 <jens@burla.dk>
2015-12-08 10:28:14 +0100

    Added getRandomValue to  crossoverSelection

--- code/geneticProcess/crossoverSelection.c
+++ code/geneticProcess/crossoverSelection.c
@@ -1,6 +1,6 @@
 #include "crossoverSelection.h"
 
 void crossoverSelectionRandom(PopMember population[], int currentPopulationSize, PopMember parent1, PopMember parent2) {
-  parent1 = population[rand() % currentPopulationSize],
-  parent2 = population[rand() % currentPopulationSize];
+  parent1 = population[getRandomValue(currentPopulationSize)],
+  parent2 = population[getRandomValue(currentPopulationSize)];
 }
