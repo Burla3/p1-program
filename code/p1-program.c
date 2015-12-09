516bf9cc299fb041e539ce1835379d821eb66848
Mathias Nielsen <lynge@lynges.net>
2015-12-09 10:11:49 +0100

    Fixed prints from main

--- code/p1-program.c
+++ code/p1-program.c
@@ -70,11 +70,11 @@ int main(int argc, const char *argv[]) {
   }
 
   printf("---------------------------------------\n");
-  printf("\nGeneration: %d\nFitness: %d\n", generation, population[i].fitnessScore);
+  printf("\nGeneration: %d\nFitness: %d\n", generation, population[0].fitnessScore);
   for (j = 0; j < numberOfStudies; j++) {
     printf("\n%s\n\n", population[0].studies[j].studyName);
     for (h = 0; h < population[0].studies[j].numberOfLectures; h++) {
-      printf("%s\t%s\n", population[0].studies[j].lectures[h].type, population[i].studies[j].lectures[h].room);
+      printf("%s\t%s\n", population[0].studies[j].lectures[h].type, population[0].studies[j].lectures[h].room);
     }
   }
   return 0;
